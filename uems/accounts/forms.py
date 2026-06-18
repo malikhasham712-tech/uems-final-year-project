89edd4edd4ca33f4bba2d81467aef2da7ed00a8e
Asim Javed <ajasimjaved786@gmail.com>
2026-06-19 00:03:36 +0500

    eye icon on password toggle and confirm password

--- uems/accounts/forms.py
+++ uems/accounts/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class RegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
     first_name = forms.CharField(max_length=100, required=True, label='First Name')
     last_name = forms.CharField(max_length=100, required=True, label='Last Name')
     role = forms.ChoiceField(choices=[
@@ -13,3 +14,13 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            self.add_error('confirm_password', 'Passwords do not match.')
+
+        return cleaned_data
